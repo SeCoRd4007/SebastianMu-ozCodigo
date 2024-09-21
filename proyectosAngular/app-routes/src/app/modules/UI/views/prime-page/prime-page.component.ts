788fef8199e4562a7aa227665a0f37d285e625a1
SeCoRd4007 <sebas_mr2000@udenar.edu.co>
2024-09-21 10:12:07 -0500

    commit: ª

---
+++ proyectosAngular/app-routes/src/app/modules/UI/views/prime-page/prime-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prime-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './prime-page.component.html',
+  styleUrl: './prime-page.component.css'
+})
+export class PrimePageComponent {
+
+}
--- proyectosAngular/app-routes/src/app/modules/home/home.component.ts
+++ proyectosAngular/app-routes/src/app/modules/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthComponent } from "../auth/auth.component";
+import { SidebarComponent } from "../sidebar/sidebar.component";
+import { SidetopPageComponent } from "../sidetop/views/sidetop-page/sidetop-page.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AuthComponent, SidebarComponent, SidetopPageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
